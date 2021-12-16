3b59841f5452376ae51a9aad6e6870e45b3d7a18
cwaku <rickymormor1@gmail.com>
2021-12-16 12:38:22 +0000

    Create, Get, Add data using API

--- src/fetch.js
+++ src/fetch.js
@@ -1,6 +1,33 @@
-import data from './data.js';
+const userInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
 
-// fetch data from data.js
-const fetch = () => data;
+const sendNewItem = () => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name: 'Avalanche Pipes' }),
+  });
+};
 
-export default fetch;
+const sendNewScore = () => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/nONLcYfe5VIS7yt3lQIO/scores/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user: userInput.value, score: scoreInput.value }),
+  });
+};
+
+const getScores = async () => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/nONLcYfe5VIS7yt3lQIO/scores/';
+  const data = await fetch(url);
+  const content = await data.json();
+  return content.result;
+};
+
+export { sendNewItem, sendNewScore, getScores };
