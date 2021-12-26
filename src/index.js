3ea209b17f3ca5b877dc4bc595fe6d3b9559ada8
Restyled.io <commits@restyled.io>
2021-12-26 08:34:21 +0000

    Restyled by prettier

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import './style.css';
-import './animation.css';
+import "./style.css";
+import "./animation.css";
 
-import {sendNewScore} from './fetch.js';
-import render from './getDOM.js';
+import { sendNewScore } from "./fetch.js";
+import render from "./getDOM.js";
 
-const refreshButton = document.querySelector('.refresh-button');
-const form = document.getElementById('form');
+const refreshButton = document.querySelector(".refresh-button");
+const form = document.getElementById("form");
 
-document.addEventListener('DOMContentLoaded', () => {
-  refreshButton.addEventListener('click', async () => { render(); });
+document.addEventListener("DOMContentLoaded", () => {
+  refreshButton.addEventListener("click", async () => {
+    render();
+  });
 
-  form.addEventListener('submit', async (event) => {
+  form.addEventListener("submit", async (event) => {
     event.preventDefault();
-    const name = document.querySelector('.name-input');
-    const score = document.querySelector('.score-input');
+    const name = document.querySelector(".name-input");
+    const score = document.querySelector(".score-input");
     sendNewScore();
-    name.value = '';
-    score.value = '';
+    name.value = "";
+    score.value = "";
   });
 
   render();
