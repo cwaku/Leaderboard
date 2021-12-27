7b102b168b5dab608de911c35b700e59ba65f1ee
cwaku <rickymormor1@gmail.com>
2021-12-27 01:38:40 +0000

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import './style.css';
 import './animation.css';
+
 import { sendNewScore } from './fetch.js';
 import render from './getDOM.js';
 
-const refreshButton = document.querySelector('.refresh-button');
-const form = document.getElementById('form');
-
 document.addEventListener('DOMContentLoaded', () => {
-  refreshButton.addEventListener('click', async () => {
-    render();
-  });
+  // Add event listeners to the buttons
+  document.querySelector('.refresh-button').addEventListener('click', async () => { render(); });
 
-  form.addEventListener('submit', async (event) => {
+  document.getElementById('form').addEventListener('submit', async (event) => {
     event.preventDefault();
-    const name = document.querySelector('.name-input');
-    const score = document.querySelector('.score-input');
     sendNewScore();
-    name.value = '';
-    score.value = '';
+    document.querySelector('.name-input').value = '';
+    document.querySelector('.score-input').value = '';
   });
 
   render();
